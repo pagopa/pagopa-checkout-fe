b5dd039da354804be6805db1d10310b2e835ca00
Nazar Chekalin <nazcheck@gmail.com>
2022-03-01 11:37:31 +0100

    Add ko page

---
+++ io-pay-portal-fe/src/routes/KOPage.tsx
@@ -0,0 +1,68 @@
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import ko from "../assets/images/response-umbrella.svg";
+import PageContainer from "../components/PageContent/PageContainer";
+import { resetCheckData } from "../redux/slices/checkData";
+import { onBrowserUnload } from "../utils/eventListeners";
+
+export default function KOPage() {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    dispatch(resetCheckData());
+    window.removeEventListener("beforeunload", onBrowserUnload);
+  }, []);
+  sessionStorage.clear();
+
+  return (
+    <PageContainer>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        sx={{ mt: 6 }}
+      >
+        <img
+          src={ko}
+          alt="ko-image"
+          style={{ width: "80px", height: "80px" }}
+        />
+        <Box
+          mt={3}
+          mb={3}
+          gap={2}
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography variant="h6" component="div">
+            {t("koPage.title")}
+          </Typography>
+          <Typography variant="body2" component="div">
+            {t("koPage.body")}
+          </Typography>
+        </Box>
+        <Box pr={8} pl={8} sx={{ width: "100%", height: "100%" }}>
+          <Button
+            type="button"
+            variant="outlined"
+            onClick={() => navigate("/")}
+            style={{
+              width: "100%",
+              height: "100%",
+              minHeight: 45,
+            }}
+          >
+            {t("koPage.button")}
+          </Button>
+        </Box>
+      </Box>
+    </PageContainer>
+  );
+}
