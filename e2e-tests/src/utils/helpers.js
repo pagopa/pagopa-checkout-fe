a7fcdda4448a5ee82916059af3fddbedbf1c556d
MarcoAGreco <marcoandreagreco@gmail.com>
2022-03-03 12:14:38 +0100

    [IOPAY-455] add happy path e2e test (verify)

--- e2e-tests/src/utils/helpers.js
+++ e2e-tests/src/utils/helpers.js
@@ -1,12 +1,16 @@
-export const selectForm = async (noticeCode, fiscalCode) => {
+export const selectKeyboardForm = async () => {
   const selectFormBtn = '#keyboardFormBtn';
-  const noticeCodeTextInput = '#billCode';
-  const fiscalCodeTextInput = '#cf';
-  const verifyBtnXPath = '//*[@id="mui-3"]';
 
   await page.waitForSelector(selectFormBtn);
   await page.click(selectFormBtn);
+};
 
+export const fillPaymentNotificationForm = async (noticeCode, fiscalCode) => {
+  const noticeCodeTextInput = '#billCode';
+  const fiscalCodeTextInput = '#cf';
+  const verifyBtnXPath = '/html/body/div/div/div[2]/div/div[2]/form/div[2]/div[2]/button';
+
+  await selectKeyboardForm();
   await page.waitForSelector(noticeCodeTextInput);
   await page.click(noticeCodeTextInput);
   await page.keyboard.type(noticeCode);
@@ -17,4 +21,24 @@ export const selectForm = async (noticeCode, fiscalCode) => {
 
   const verifyBtnElem = await page.waitForXPath(verifyBtnXPath);
   await verifyBtnElem.click();
+};
+
+export const verifyPaymentAndGetError = async (noticeCode, fiscalCode) => {
+  const errorMessageXPath = '/html/body/div[2]/div[3]/div/div/div/div[1]';
+
+  await fillPaymentNotificationForm(noticeCode, fiscalCode);
+  const errorMessageElem = await page.waitForXPath(errorMessageXPath);
+
+  return await errorMessageElem.evaluate(el => el.textContent);
+};
+
+export const verifyPayment = async (noticeCode, fiscalCode) => {
+  await fillPaymentNotificationForm(noticeCode, fiscalCode);
+
+  page.on('response', response => {
+    const status = response.status();
+    expect(status).toBe(304);
+
+    expect(response.url).toContain('/payment/summary');
+  });
 };
