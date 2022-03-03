94ca380ad7822f7a109f53d185ee1018beacf7d8
MarcoAGreco <marcoandreagreco@gmail.com>
2022-03-03 12:14:38 +0100

    [IOPAY-455] add happy path e2e test (verify)

--- e2e-tests/src/checkout.activation.test.js
+++ e2e-tests/src/checkout.activation.test.js
@@ -1,14 +1,11 @@
-import { selectForm, verifyPaymen } from './utils/helpers.js';
+import { verifyPayment } from './utils/helpers.js';
 
 describe('Checkout payment activation tests', () => {
   /**
    * Test input and configuration
    */
   const CHECKOUT_URL = 'http://localhost:1234/'; // TODO: update to DEV/UAT env
-  const PAID_NOTICE_CODE = '002720356866984253';
-  const PAID_FISCAL_CODE_PA = '01199250158';
-  const INVALID_NOTICE_CODE = '002720356512737900';
-  const INVALID_FISCAL_CODE_PA = '77777777799';
+  const VALID_FISCAL_CODE = '77777777777';
   const VALID_NOTICE_CODE = Math.floor(
     Math.random() * (302001999999999999 - 302001000000000000 + 1) + 302001000000000000,
   ).toString();
@@ -24,11 +21,10 @@ describe('Checkout payment activation tests', () => {
     await page.setViewport({ width: 1200, height: 907 });
   });
 
-  it('Should return a message indicating duplicate payment if the notice_code has already been paid', async () => {
+  it('Should correctly verify a payment notification', async () => {
     /**
      * 1. Payment Activation with paid notice code
      *  */
-    await selectForm(PAID_NOTICE_CODE, PAID_FISCAL_CODE_PA);
-    await new Promise(r => setTimeout(r, 10000));
+    await verifyPayment(VALID_NOTICE_CODE, VALID_FISCAL_CODE);
   });
 });
