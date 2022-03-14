62fab5449a01ddb09584bd42c93bb399bdeff2cb
MarcoAGreco <marcoandreagreco@gmail.com>
2022-03-14 10:04:06 +0100

    [IOPAY-455] add: complete payment e2e test

--- e2e-tests/src/checkout.activation.test.js
+++ e2e-tests/src/checkout.activation.test.js
@@ -1,4 +1,4 @@
-import { verifyPayment } from './utils/helpers.js';
+import { payNotice, verifyPayment } from './utils/helpers.js';
 
 describe('Checkout payment activation tests', () => {
   /**
@@ -6,6 +6,14 @@ describe('Checkout payment activation tests', () => {
    */
   const CHECKOUT_URL = 'http://localhost:1234/'; // TODO: update to DEV/UAT env
   const VALID_FISCAL_CODE = '77777777777';
+  const INVALID_FISCAL_CODE = '07777777779';
+  const EMAIL = 'mario.rossi@email.com';
+  const VALID_CARD_DATA = {
+    number: '4801769871971639',
+    expirationDate: '1230',
+    ccv: '123',
+    holderName: 'Mario Rossi',
+  };
   const VALID_NOTICE_CODE = Math.floor(
     Math.random() * (302001999999999999 - 302001000000000000 + 1) + 302001000000000000,
   ).toString();
@@ -14,17 +22,36 @@ describe('Checkout payment activation tests', () => {
    * Increase default test timeout (5000ms)
    * to support entire payment flow
    */
-  jest.setTimeout(30000);
+  jest.setTimeout(50000);
 
   beforeEach(async () => {
     await page.goto(CHECKOUT_URL);
     await page.setViewport({ width: 1200, height: 907 });
   });
 
+  /*
   it('Should correctly verify a payment notification', async () => {
     /**
-     * 1. Payment Activation with paid notice code
-     *  */
+     * 1. Payment Activation with valid notice code
+     *  
     await verifyPayment(VALID_NOTICE_CODE, VALID_FISCAL_CODE);
+  });
+
+  /*
+  it('Should fail the verify of a malformed rptId', async () => {
+    const errorMessage = await verifyPaymentAndGetError(VALID_NOTICE_CODE, INVALID_FISCAL_CODE);
+    // eslint-disable-next-line no-console
+    console.log(JSON.stringify(errorMessage));
+  });
+  */
+
+  it('Should correctly execute a payment', async () => {
+    /*
+     * 2. Payment with valid notice code
+     */
+    await verifyPayment(VALID_NOTICE_CODE, VALID_FISCAL_CODE);
+    const resultMessage = await payNotice(EMAIL, VALID_CARD_DATA);
+
+    expect(resultMessage).toContain('Grazie, hai pagato');
   });
 });
--- e2e-tests/src/utils/helpers.js
+++ e2e-tests/src/utils/helpers.js
@@ -26,6 +26,7 @@ export const fillPaymentNotificationForm = async (noticeCode, fiscalCode) => {
 export const verifyPaymentAndGetError = async (noticeCode, fiscalCode) => {
   const errorMessageXPath = '/html/body/div[2]/div[3]/div/div/div/div[1]';
 
+  await acceptCookiePolicy();
   await fillPaymentNotificationForm(noticeCode, fiscalCode);
   const errorMessageElem = await page.waitForXPath(errorMessageXPath);
 
@@ -33,12 +34,92 @@ export const verifyPaymentAndGetError = async (noticeCode, fiscalCode) => {
 };
 
 export const verifyPayment = async (noticeCode, fiscalCode) => {
+  await acceptCookiePolicy();
   await fillPaymentNotificationForm(noticeCode, fiscalCode);
+};
 
-  page.on('response', response => {
-    const status = response.status();
-    expect(status).toBe(304);
+export const acceptCookiePolicy = async () => {
+  const acceptPolicyBtn = '#onetrust-accept-btn-handler';
+  const darkFilterXPath = '/html/body/div[2]/div[1]';
 
-    expect(response.url).toContain('/payment/summary');
-  });
+  await page.waitForSelector(acceptPolicyBtn);
+  await page.click(acceptPolicyBtn);
+
+  // Avoid click on form button when dark filter is still enabled
+  await page.waitForXPath(darkFilterXPath, { hidden: true });
+};
+
+export const payNotice = async (email, cardData) => {
+  const payNoticeBtnXPath = '/html/body/div[1]/div/div[2]/div/div[6]/div[2]/button';
+  const resultMessageXPath = '/html/body/div[1]/div/div[2]/div/div/div/h6';
+
+  const payNoticeBtn = await page.waitForXPath(payNoticeBtnXPath);
+  await payNoticeBtn.click();
+
+  await fillEmailForm(email);
+  await choosePaymentMethod('card');
+
+  await fillCardDataForm(cardData);
+  const message = await page.waitForXPath(resultMessageXPath);
+  return await message.evaluate(el => el.textContent);
+};
+
+export const fillEmailForm = async email => {
+  const emailInput = '#email';
+  const confirmEmailInput = '#confirmEmail';
+  const continueBtnXPath = '/html/body/div[1]/div/div[2]/div/div[2]/form/div[2]/div[2]';
+
+  await page.waitForSelector(emailInput);
+  await page.click(emailInput);
+  await page.keyboard.type(email);
+
+  await page.waitForSelector(confirmEmailInput);
+  await page.click(confirmEmailInput);
+  await page.keyboard.type(email);
+
+  const continueBtn = await page.waitForXPath(continueBtnXPath);
+  await continueBtn.click();
+};
+
+export const choosePaymentMethod = async method => {
+  const cardOptionXPath = '/html/body/div[1]/div/div[2]/div/div[2]/div[1]/div[1]/div/div';
+
+  switch (method) {
+    case 'card': {
+      const cardOptionBtn = await page.waitForXPath(cardOptionXPath);
+      await cardOptionBtn.click();
+      break;
+    }
+  }
+};
+
+export const fillCardDataForm = async cardData => {
+  const cardNumberInput = '#number';
+  const expirationDateInput = '#expirationDate';
+  const ccvInput = '#cvv';
+  const holderNameInput = '#name';
+  const continueBtnXPath = '/html/body/div[1]/div/div[2]/div/div[2]/form/div[2]/div[2]';
+  const payBtnXPath = '/html/body/div[1]/div/div[2]/div/div[7]/div[2]/button';
+
+  await page.waitForSelector(cardNumberInput);
+  await page.click(cardNumberInput);
+  await page.keyboard.type(cardData.number);
+
+  await page.waitForSelector(expirationDateInput);
+  await page.click(expirationDateInput);
+  await page.keyboard.type(cardData.expirationDate);
+
+  await page.waitForSelector(ccvInput);
+  await page.click(ccvInput);
+  await page.keyboard.type(cardData.ccv);
+
+  await page.waitForSelector(holderNameInput);
+  await page.click(holderNameInput);
+  await page.keyboard.type(cardData.holderName);
+
+  const continueBtn = await page.waitForXPath(continueBtnXPath);
+  await continueBtn.click();
+
+  const payBtn = await page.waitForXPath(payBtnXPath);
+  await payBtn.click();
 };
