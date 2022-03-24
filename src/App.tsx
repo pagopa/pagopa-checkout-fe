98a5d614ce18fb8b35bd759ce0dfe09a9e9a388b
Nazar Chekalin <nazcheck@gmail.com>
2022-03-24 09:57:38 +0100

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Guard from "./components/commons/Guard";
 import { Layout } from "./components/commons/Layout";
 import CancelledPage from "./routes/CancelledPage";
 import DonationPage from "./routes/DonationPage";
-import ShortcutNoticePage from "./routes/ShortcutNoticePage";
 import IndexPage from "./routes/IndexPage";
 import InputCardPage from "./routes/InputCardPage";
 import KOPage from "./routes/KOPage";
@@ -163,7 +162,7 @@ export function App() {
               />
               <Route path="cancelled" element={<CancelledPage />} />
               <Route path="ko" element={<KOPage />} />
-              <Route path=":rptid" element={<ShortcutNoticePage />} />
+              <Route path=":rptid" element={<PaymentNoticePage />} />
               <Route path="*" element={<Navigate replace to="/" />} />
             </Route>
             <Route path="*" element={<Navigate replace to="/" />} />
--- src/routes/PaymentNoticePage.tsx
+++ src/routes/PaymentNoticePage.tsx
@@ -1,8 +1,8 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, CircularProgress } from "@mui/material";
 import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import { useTranslation } from "react-i18next";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import * as TE from "fp-ts/TaskEither";
 import { pipe } from "fp-ts/function";
 import { RptId } from "../../generated/definitions/payment-activations-api/RptId";
@@ -28,6 +28,7 @@ export default function PaymentNoticePage() {
   const { t } = useTranslation();
   const location = useLocation();
   const navigate = useNavigate();
+  const { rptid } = useParams();
   const noticeInfo = getNoticeInfo();
   const currentPath = location.pathname.split("/")[1];
 
@@ -36,6 +37,7 @@ export default function PaymentNoticePage() {
   const [errorModalOpen, setErrorModalOpen] = React.useState(false);
   const [error, setError] = React.useState("");
   const [loading, setLoading] = React.useState(false);
+  const [loadingShortcut] = React.useState(!!rptid);
 
   const onError = (m: string) => {
     setLoading(false);
@@ -55,8 +57,7 @@ export default function PaymentNoticePage() {
         TE.map((paymentInfo) => {
           setPaymentInfo(paymentInfo as PaymentInfo);
           setRptId(notice);
-          setLoading(false);
-          navigate(`/${currentPath}/summary`);
+          navigate(`/${currentPath}/summary`, rptid ? { replace: true } : {});
         })
       )();
     },
@@ -67,48 +68,73 @@ export default function PaymentNoticePage() {
     navigate(-1);
   };
 
-  return (
-    <PageContainer
-      title="paymentNoticePage.title"
-      description="paymentNoticePage.description"
-    >
-      <Button
-        variant="text"
-        onClick={() => setModalOpen(true)}
-        sx={{ p: 0 }}
-        aria-hidden="true"
-        tabIndex={-1}
-      >
-        {t("paymentNoticePage.helpLink")}
-      </Button>
-      <Box sx={{ mt: 6 }}>
-        <PaymentNoticeForm
-          onCancel={onCancel}
-          onSubmit={onSubmit}
-          defaultValues={noticeInfo}
-          loading={loading}
-        />
-      </Box>
+  React.useEffect(() => {
+    if (rptid) {
+      void onSubmit({
+        billCode: rptid?.slice(11) || "",
+        cf: rptid?.slice(0, 11) || "",
+      });
+    }
+  }, [rptid]);
 
-      <InformationModal
-        open={modalOpen}
-        onClose={() => {
-          setModalOpen(false);
-        }}
-      >
-        <img
-          src={notification}
-          alt="facsimile"
-          style={useSmallDevice() ? { width: "100%" } : { height: "80vh" }}
-        />
-      </InformationModal>
-      <ErrorModal
-        error={error}
-        open={errorModalOpen}
-        onClose={() => {
-          setErrorModalOpen(false);
-        }}
-      />
+  return (
+    <>
+      {loadingShortcut ? (
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          my={10}
+          aria-live="assertive"
+          aria-label={t("ariaLabels.loading")}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <PageContainer
+          title="paymentNoticePage.title"
+          description="paymentNoticePage.description"
+        >
+          <Button
+            variant="text"
+            onClick={() => setModalOpen(true)}
+            sx={{ p: 0 }}
+            aria-hidden="true"
+            tabIndex={-1}
+          >
+            {t("paymentNoticePage.helpLink")}
+          </Button>
+          <Box sx={{ mt: 6 }}>
+            <PaymentNoticeForm
+              onCancel={onCancel}
+              onSubmit={onSubmit}
+              defaultValues={noticeInfo}
+              loading={loading}
+            />
+          </Box>
+
+          <InformationModal
+            open={modalOpen}
+            onClose={() => {
+              setModalOpen(false);
+            }}
+          >
+            <img
+              src={notification}
+              alt="facsimile"
+              style={useSmallDevice() ? { width: "100%" } : { height: "80vh" }}
+            />
+          </InformationModal>
+          <ErrorModal
+            error={error}
+            open={errorModalOpen}
+            onClose={() => {
+              setErrorModalOpen(false);
+            }}
+          />
+        </PageContainer>
+      )}
       <Box display="none">
         <ReCAPTCHA
           ref={ref}
@@ -116,6 +142,6 @@ export default function PaymentNoticePage() {
           sitekey={getReCaptchaKey() as string}
         />
       </Box>
-    </PageContainer>
+    </>
   );
 }
