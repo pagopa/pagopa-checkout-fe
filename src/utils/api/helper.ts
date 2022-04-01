e94f07de3035c9810355fe6c5ba58c35de0ea909
Simone Infante <simone.infante@pagopa.it>
2022-04-01 18:40:48 +0200

    fix 3ds data

--- src/utils/api/helper.ts
+++ src/utils/api/helper.ts
@@ -847,7 +847,7 @@ export const confirmPayment = async (
     deliveryEmailAddress: pipe(
       O.fromNullable(sessionStorage.getItem("useremail")),
       O.getOrElse(() => ""),
-      (_) => _.replace('"', "")
+      (_) => _.replaceAll('"', "")
     ),
     mobilePhone: null,
   };
