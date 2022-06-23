bde78273d6c01549fbb0c8fa1e6820db53753200
Vito Falco <vitofalco@gmail.com>
2022-06-23 13:05:23 +0200

    Shell script to generate a folder in dist for OT

---
+++ ot.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Useful to copy the "right" OneTrust folder in dist
+# id in PROD we use "production", otherwise "test"
+
+# if env is not setted
+if [[ -z "${env}" ]]; then
+  OT_FOLDER="test"
+else
+  OT_FOLDER=$([[ "$env" == "PROD" ]] && echo "production" || echo "test")
+fi
+cp -R "ot/$OT_FOLDER" "dist/ot"
