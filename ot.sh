b718858b529fab225d0a186b5dd4f46444c091c0
Simone Infante <simone.infante@pagopa.it>
2022-06-24 15:27:16 +0200

    test 3

--- ot.sh
+++ ot.sh
@@ -3,11 +3,13 @@
 # id in PROD we use "production", otherwise "test"
 
 # if env is not setted
-if [[ -z "${env}" ]]; then
+if [[ -z "$env" ]]; then
   OT_FOLDER="test"
 else
   OT_FOLDER=$([[ "$env" == "PROD" ]] && echo "production" || echo "test")
 fi
+echo "./ot/$OT_FOLDER"
+echo $env
 echo "./ot/$OT_FOLDER"
 ls ./ot/$OT_FOLDER
 cp -R "./ot/$OT_FOLDER" "./dist/ot"
