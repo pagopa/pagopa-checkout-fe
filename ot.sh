fde62f2784ac3fb61864619a7f8066d8efbc1f72
Simone Infante <simone.infante@pagopa.it>
2022-06-28 16:50:14 +0200

    add echo to debug

--- ot.sh
+++ ot.sh
@@ -3,9 +3,15 @@
 # id in PROD we use "production", otherwise "test"
 
 # if env is not setted
+echo $CHECKOUT_ENV
+
 if [[ -z $CHECKOUT_ENV ]]; then
   OT_FOLDER="test"
 else
   OT_FOLDER=$([[ $CHECKOUT_ENV == "PROD" ]] && echo "production" || echo "test")
 fi
+echo $CHECKOUT_ENV
+echo "./ot/$OT_FOLDER"
+echo $OT_FOLDER
+
 cp -R "./ot/$OT_FOLDER" "./dist/ot"
