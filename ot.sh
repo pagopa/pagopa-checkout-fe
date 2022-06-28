7045cb517236c0cf0a70b48701d62ef28dbe22ff
Simone Infante <simone.infante@pagopa.it>
2022-06-28 16:41:22 +0200

    fix: env name

--- ot.sh
+++ ot.sh
@@ -3,9 +3,9 @@
 # id in PROD we use "production", otherwise "test"
 
 # if env is not setted
-if [[ -z "$CHECKOUT_ENV" ]]; then
+if [[ -z $CHECKOUT_ENV ]]; then
   OT_FOLDER="test"
 else
-  OT_FOLDER=$([[ "$CHECKOUT_ENV" == "PROD" ]] && echo "production" || echo "test")
+  OT_FOLDER=$([[ $CHECKOUT_ENV == "PROD" ]] && echo "production" || echo "test")
 fi
 cp -R "./ot/$OT_FOLDER" "./dist/ot"
