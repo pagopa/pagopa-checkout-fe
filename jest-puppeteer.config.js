29e8291b7e91ccd15592f0b5eb8a9875d2668882
Nazar Chekalin <nazcheck@gmail.com>
2023-01-20 11:57:42 +0100

    fix

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     launch: {
         dumpio: true,
-        headless: false,
+        headless: true,
         product: 'chrome',
         args: ["--no-sandbox"] // to fix
     },
