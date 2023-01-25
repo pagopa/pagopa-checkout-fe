fc13044b5491e6112fd784476a240b5e34cbf097
Nazar Chekalin <nazcheck@gmail.com>
2023-01-25 14:53:53 +0100

    Add only digits handle change

--- src/features/payment/components/PaymentNoticeForm/PaymentNoticeForm.tsx
+++ src/features/payment/components/PaymentNoticeForm/PaymentNoticeForm.tsx
@@ -46,6 +46,15 @@ export function PaymentNoticeForm(props: {
     return errors;
   };
 
+  const handleValidChange =
+    (handleChange: (e: React.ChangeEvent<any>) => void) =>
+    (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      e.currentTarget.value = cleanSpaces(e.currentTarget.value);
+      if (/^[0-9]*$/.test(e.currentTarget.value)) {
+        handleChange(e);
+      }
+    };
+
   return (
     <>
       <Formik
@@ -79,10 +88,7 @@ export function PaymentNoticeForm(props: {
                 type="text"
                 inputMode="numeric"
                 value={values.billCode}
-                handleChange={(e) => {
-                  e.currentTarget.value = cleanSpaces(e.currentTarget.value);
-                  handleChange(e);
-                }}
+                handleChange={handleValidChange(handleChange)}
                 handleBlur={handleBlur}
                 sx={{ mb: 4 }}
                 endAdornment={
@@ -104,10 +110,7 @@ export function PaymentNoticeForm(props: {
                 type="text"
                 inputMode="numeric"
                 value={values.cf}
-                handleChange={(e) => {
-                  e.currentTarget.value = cleanSpaces(e.currentTarget.value);
-                  handleChange(e);
-                }}
+                handleChange={handleValidChange(handleChange)}
                 handleBlur={handleBlur}
                 endAdornment={
                   <InputAdornment position="end">
