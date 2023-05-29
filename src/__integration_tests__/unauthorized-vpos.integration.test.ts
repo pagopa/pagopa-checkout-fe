cb36f42e51ec2eb67a19b312b72872afd7828ec5
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-05-29 15:46:14 +0200

    different xpay vpos unaouthorized test suite. 30 minute timeout.

--- src/__integration_tests__/unauthorized-vpos.integration.test.ts
+++ src/__integration_tests__/unauthorized-vpos.integration.test.ts
@@ -17,21 +17,6 @@ const VALID_CARD_DATA = {
   holderName: "Mario Rossi",
 };
 
-
-  const UNATHORIZED_XPAY = "302016723740100072";
-  const UNATHORIZED_XPAY_INVALID_CARD_22 = "302016723740102272";
-const UNATHORIZED_XPAY_EXPIRED_CARD_14 = "302016723740101472";
-const UNATHORIZED_XPAY_ECARD_BRAND_NOT_PERMITTED_15 = "302016723740101572";
-
-const UNATHORIZED_XPAY_DUPLICATE_TRANSACTION_9 = "302016723740100972";
-const UNATHORIZED_XPAY_FORBIDDEN_OPERATION_99 = "302016723740109972";
-const UNATHORIZED_XPAY_UNAVAILABLE_METHOD_98 = "302016723740109872";
-const UNATHORIZED_XPAY_KO_RETRIABLE_96 = "302016723740109672";
-const UNATHORIZED_XPAY_INTERNAL_ERROR_100 = "302016723740110072";
-const UNATHORIZED_XPAY_INVALID_STATUS_16 = "302016723740101672";
-
-const UNATHORIZED_XPAY_CANCELED_3DS_AUTH_20 = "302016723740102072";
-
 const UNATHORIZED_VPOS = "302016723740200072";
 const UNATHORIZED_VPOS_TIMEOUT_21 = "302016723740202172";
 const UNATHORIZED_VPOS_REQREFNUM_INVALID_02 = "302016723740200272";
@@ -84,126 +69,7 @@ const UNATHORIZED_VPOS_PAYMENT_INSTRUMENT_NOT_ACCEPTED_35 = "302016723740203572"
   beforeEach(async () => {
     await page.goto(CHECKOUT_URL);
   });
-  
-it("Should fails - final status UNATHORIZED with XPAY error code INVALID_CARD 22", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY_INVALID_CARD_22,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
 
-  expect(resultMessage).toContain("C’è un problema con la tua carta");
-});
-
-it("Should fails - final status UNATHORIZED with XPAY error code EXPIRED_CARD 14", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY_EXPIRED_CARD_14,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
-
-  expect(resultMessage).toContain("C’è un problema con la tua carta");
-});
-
-it("Should fails - final status UNATHORIZED with XPAY error code ECARD_BRAND_NOT_PERMITTED 15", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY_ECARD_BRAND_NOT_PERMITTED_15,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
-
-  expect(resultMessage).toContain("C’è un problema con la tua carta");
-});
-
-it("Should fails - final status UNATHORIZED with XPAY error code DUPLICATE_TRANSACTION 9", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY_DUPLICATE_TRANSACTION_9,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
-
-  expect(resultMessage).toContain("Spiacenti, si è verificato un errore imprevisto");
-});
-
-it("Should fails - final status UNATHORIZED with XPAY error code FORBIDDEN_OPERATION 99", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY_FORBIDDEN_OPERATION_99,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
-
-  expect(resultMessage).toContain("Spiacenti, si è verificato un errore imprevisto");
-});
-
-it("Should fails - final status UNATHORIZED with XPAY error code INTERNAL_ERROR 100", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY_INTERNAL_ERROR_100,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
-
-  expect(resultMessage).toContain("Spiacenti, si è verificato un errore imprevisto");
-});
-
-it("Should fails - final status UNATHORIZED with XPAY error code KO_RETRIABLE 96", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY_KO_RETRIABLE_96,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
-
-  expect(resultMessage).toContain("Spiacenti, si è verificato un errore imprevisto");
-});
-
-it("Should fails - final status UNATHORIZED with XPAY error code UNAVAILABLE_METHOD 98", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY_UNAVAILABLE_METHOD_98,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
-
-  expect(resultMessage).toContain("Spiacenti, si è verificato un errore imprevisto");
-});
-
-it("Should fails - final status UNATHORIZED with XPAY error code INVALID_STATUS 16", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY_INVALID_STATUS_16,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
-
-  expect(resultMessage).toContain("Spiacenti, si è verificato un errore imprevisto");
-});
-
-it("Should fails - final status UNATHORIZED with XPAY error code CANCELED_3DS_AUTH 20", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY_CANCELED_3DS_AUTH_20,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
-
-  expect(resultMessage).toContain("L’operazione è stata annullata");
-});
 
 it("Should fails - final status UNATHORIZED with VPOS error code TIMEOUT 21", async () => {
   const resultMessage = await payNotice(
@@ -536,18 +402,6 @@ it("Should fails - final status UNATHORIZED with VPOS error code PAYMENT_INSTRUM
   );
 
   expect(resultMessage).toContain("C’è un problema con la tua carta");
-});
-
-it("Should fails - final status UNATHORIZED with XPAY no error code provided", async () => {
-  const resultMessage = await payNotice(
-    UNATHORIZED_XPAY,
-    VALID_FISCAL_CODE,
-    EMAIL,
-    VALID_CARD_DATA,
-    CHECKOUT_URL_AFTER_AUTHORIZATION
-  );
-
-  expect(resultMessage).toContain("Spiacenti, si è verificato un errore imprevisto");
 });
 
 it("Should fails - final status UNATHORIZED with VPOS no error code provided", async () => {
