4773715a74080792ab487285be0f05abafb07a50
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-07-14 14:57:01 +0200

    feat(CHK-1690): small feedbacks

--- src/components/commons/SurveyLink.tsx
+++ src/components/commons/SurveyLink.tsx
@@ -1,5 +1,5 @@
 import { default as React } from "react";
-import { Typography, Alert, AlertTitle, Link } from "@mui/material";
+import { Typography, Alert, AlertTitle, Link, Box } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "@pagopa/mui-italia";
@@ -9,33 +9,26 @@ const SurveyLink = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Alert
-        icon={false}
-        severity="info"
-        variant="standard"
-        sx={{
-          borderLeftColor: theme.palette.info.main + " !important",
-          borderLeft: "4px solid",
-          padding: "16px",
-        }}
-      >
-        <AlertTitle>
-          <Typography variant="body1" sx={{ fontWeight: 600 }} mb={1}>
-            {t("paymentResponsePage.survey.title")}
+      <Alert icon={false} severity="info" variant="standard">
+        <Box p={2}>
+          <AlertTitle>
+            <Typography variant="body1" sx={{ fontWeight: 600 }} mb={1}>
+              {t("paymentResponsePage.survey.title")}
+            </Typography>
+          </AlertTitle>
+          <Typography variant="body1" mb={2}>
+            {t("paymentResponsePage.survey.body")}
           </Typography>
-        </AlertTitle>
-        <Typography variant="body1" mb={2}>
-          {t("paymentResponsePage.survey.body")}
-        </Typography>
-        <Link
-          variant="body1"
-          target="_blank"
-          rel="nofollow"
-          href={t("paymentResponsePage.survey.link.href")}
-          sx={{ textDecoration: "none", fontWeight: 700 }}
-        >
-          {t("paymentResponsePage.survey.link.text")}
-        </Link>
+          <Link
+            variant="body1"
+            target="_blank"
+            rel="nofollow"
+            href={t("paymentResponsePage.survey.link.href")}
+            sx={{ textDecoration: "none", fontWeight: 700 }}
+          >
+            {t("paymentResponsePage.survey.link.text")}
+          </Link>
+        </Box>
       </Alert>
     </ThemeProvider>
   );
