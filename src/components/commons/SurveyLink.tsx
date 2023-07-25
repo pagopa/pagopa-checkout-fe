bd27550a86fcd21ebe52b006fbc9771b65706450
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-07-25 09:47:40 +0200

    feat(CHK-1690): added mixpanel event

--- src/components/commons/SurveyLink.tsx
+++ src/components/commons/SurveyLink.tsx
@@ -3,9 +3,20 @@ import { Typography, Alert, AlertTitle, Link, Box } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "@pagopa/mui-italia";
+import { VOC_USER_EXIT } from "../../utils/config/mixpanelDefs";
+import { mixpanel } from "../../utils/config/mixpanelHelperInit";
 
 const SurveyLink = () => {
   const { t } = useTranslation();
+
+  const onClick = () => {
+    mixpanel.track(VOC_USER_EXIT.value, {
+      EVENT_ID: VOC_USER_EXIT.value,
+      event_category: "UX",
+      event_type: "exit",
+      screen_name: "payment response page",
+    });
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -20,6 +31,7 @@ const SurveyLink = () => {
             {t("paymentResponsePage.survey.body")}
           </Typography>
           <Link
+            onClick={onClick}
             variant="body1"
             target="_blank"
             rel="nofollow"
