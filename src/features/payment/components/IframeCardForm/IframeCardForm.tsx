f163dee4b41e5d187faf5204962f0db336a5613e
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-08-09 14:27:37 +0200

    feat(1789): onBuildFlowStateChange added to the buildinstance

--- src/features/payment/components/IframeCardForm/IframeCardForm.tsx
+++ src/features/payment/components/IframeCardForm/IframeCardForm.tsx
@@ -82,10 +82,8 @@ fieldformStatus.set("CARDHOLDER_NAME", initialFormStatus);
 
 // eslint-disable-next-line functional/no-let
 let formStatus = false;
-// eslint-disable-next-line functional/no-let, @typescript-eslint/ban-ts-comment
-// @ts-ignore
 // eslint-disable-next-line functional/no-let
-let setSdkBuildIstance;
+let sdkBuildIstance: { confirmData: () => void };
 
 export default function IframeCardForm(props: Props) {
   const { loading = true, onCancel, onSubmit = () => null, hideCancel } = props;
@@ -122,8 +120,9 @@ export default function IframeCardForm(props: Props) {
     void fetchForm();
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    setSdkBuildIstance = new Build({
+    sdkBuildIstance = new Build({
       onBuildSuccess(evtData: { id: IdFields }) {
+        // write some code to manage the successful data entering in the specifiedfield: evtData.id
         fieldformStatus.set(evtData.id, {
           isValid: true,
           errorCode: null,
@@ -138,6 +137,17 @@ export default function IframeCardForm(props: Props) {
         errorCode: string;
         errorMessage: string;
       }) {
+        // write some code to manage the wrongdata entering in the specifiedfield: evtData.id
+        // the action can be finely tuned based on the provided error code available at evtData.errorCode
+        // the possible casesare:
+        //   HF0001 -generic build field error
+        //   HF0002 -temporary unavailability of the service
+        //   HF0003 -session expired–the payment experience shall be restarted from the post orders/build
+        //   HF0004 -card validation error–the luhn key check on the card number was failed
+        //   HF0005 -brand not found–the card brand is not in the list of supported brands
+        //   HF0006 -expiration date exceeded–the provided card is expired
+        //   HF0007 –internal error –if the issue persists the payment has to be restarted
+        //   HF0009 –3DS GDI verification failed –the payment experience has to be stopped with failure.
         const { id, errorCode, errorMessage } = evtData;
         fieldformStatus.set(id, {
           isValid: false,
@@ -147,11 +157,39 @@ export default function IframeCardForm(props: Props) {
         formStatus = calculateFormValidStatus(fieldformStatus);
         setDummyState(Math.random);
       },
+      onConfirmError(evtData: any) {
+        // this event is returned as a consequence of the invocation of confirmData() SDK function.
+        // the possible cases are:
+        //   HF0002 –temporary unavailability of the service
+        //   HF0003 -session expired–the payment experience shall be restarted from the post orders/build
+        //   HF0007 –internal error–if the issue persists the payment has to be restarted
+        // eslint-disable-next-line no-console
+        console.log("onConfirmError", evtData);
+      },
+      onBuildFlowStateChange(
+        evtData: any,
+        state:
+          | "READY_FOR_PAYMENT"
+          | "REDIRECTED_TO_EXTERNAL_DOMAIN"
+          | "PAYMENT_COMPLETE"
+      ) {
+        // this event is returned for each state transition of the payment state machine.
+        // the possible states expressed by the value state are:
+        // READY_FOR_PAYMENT: the card data has been properly collected and it is now possible to
+        //   invoke the server to server
+        //   posthttps://{nexiDomain}/api/phoenix-0.0/psp/api/v1/build/cardData?sessionId={thesessionId}
+        //   to collect the non-PCI card information.
+        // REDIRECTED_TO_EXTERNAL_DOMAIN: when this state is provided, the browser has to be redirected to
+        //   the evtData.data.url external domain for strong customer authentication (i.e ACS URL).
+        // PAYMENT_COMPLETE: the payment experience is finished. It is required to invoke
+        //   the get https://{nexiDomain}/api/phoenix-0.0/psp/api/v1/build/state?sessionId={thesessionId}  },
+        // eslint-disable-next-line no-console
+        console.debug("onBuildFlowStateChange", evtData, state);
+      },
+      // any dependency will initialize the build istance more than one time
+      // and I think it's not a good idea. For the same reason I am not using
+      // a react state to track the form status
     });
-
-    // any dependency will initialize the build istance more than one time
-    // and I think it's not a good idea. For the same reason I am not using
-    // a react state to track the form status
   }, []);
 
   const handleSubmit = (e: React.FormEvent) => {
