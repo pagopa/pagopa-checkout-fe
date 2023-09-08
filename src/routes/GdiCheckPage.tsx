eb3a55b53f8adcd617f302c8e0d7fe1ff6cd7963
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-09-08 16:13:12 +0200

    feat(CHK-1878): nexi payment state change managed on the /gdi-check page

--- src/routes/GdiCheckPage.tsx
+++ src/routes/GdiCheckPage.tsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import PageContainer from "../components/PageContent/PageContainer";
 import CheckoutLoader from "../components/PageContent/CheckoutLoader";
 import { getFragmentParameter } from "../utils/regex/urlUtilities";
+import { CheckoutRoutes } from "./models/routeModel";
+
+export interface NpgFlowStateEvtData {
+  data: {
+    url: string;
+  };
+}
 
 const GdiCheckPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // @ts-ignore
+    new Build({
+      onBuildFlowStateChange(
+        npgFlowStateEvtData: NpgFlowStateEvtData,
+        // creare un typo condiviso e riutilizzabile
+        state: "REDIRECTED_TO_EXTERNAL_DOMAIN" | "PAYMENT_COMPLETE"
+      ) {
+        switch (state) {
+          case "PAYMENT_COMPLETE":
+            navigate(`/${CheckoutRoutes.ESITO}`);
+            break;
+          case "REDIRECTED_TO_EXTERNAL_DOMAIN":
+            // replace? href? indagare quale è migliore
+            // eslint-disable-next-line functional/immutable-data
+            window.location.replace(npgFlowStateEvtData.data.url);
+            break;
+          default:
+            // gestire l'eventualità di ricevere uno stato non gestito o previsto in questa fase
+            // console debug da rimuovere
+            console.debug(state);
+        }
+      },
+    });
+  }, []);
+
   const gdiIframeUrl = getFragmentParameter(
     window.location.href,
     "gdiIframeUrl"
   );
+
   return (
     <PageContainer>
       <CheckoutLoader />
