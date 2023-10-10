1d041ff00846606bba11ed910bfc45930376fe1b
Vito Falco <vitofalco@gmail.com>
2023-10-10 11:51:19 +0200

    Add recatpcha check to sessions and transactions

--- src/routes/IframeCardPage.tsx
+++ src/routes/IframeCardPage.tsx
@@ -1,16 +1,11 @@
 import { Box } from "@mui/material";
 import React from "react";
-import ReCAPTCHA from "react-google-recaptcha";
 import { useNavigate } from "react-router-dom";
 import * as E from "fp-ts/Either";
 import { pipe } from "fp-ts/function";
 import PageContainer from "../components/PageContent/PageContainer";
 import IframeCardForm from "../features/payment/components/IframeCardForm/IframeCardForm";
-import {
-  getReCaptchaKey,
-  getSessionItem,
-  SessionItems,
-} from "../utils/storage/sessionStorage";
+import { getSessionItem, SessionItems } from "../utils/storage/sessionStorage";
 import { NewTransactionResponse } from "../../generated/definitions/payment-ecommerce/NewTransactionResponse";
 
 export default function IFrameCardPage() {
@@ -21,7 +16,6 @@ export default function IFrameCardPage() {
   // Is It allowed to store the bin temporary?
   // const [bin, setBin] = React.useState("");
   const [hideCancelButton, setHideCancelButton] = React.useState(false);
-  const ref = React.useRef<ReCAPTCHA>(null);
   // const dispatch = useAppDispatch();
 
   React.useEffect(() => {
@@ -45,13 +39,6 @@ export default function IFrameCardPage() {
           onCancel={onCancel}
           hideCancel={hideCancelButton}
           loading={loading}
-        />
-      </Box>
-      <Box display="none">
-        <ReCAPTCHA
-          ref={ref}
-          size="invisible"
-          sitekey={getReCaptchaKey() as string}
         />
       </Box>
     </PageContainer>
