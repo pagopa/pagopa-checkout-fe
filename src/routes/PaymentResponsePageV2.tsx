a6b0dc25af4682ded1f4ee9edd7e77ab715e5281
scaminati-bv <simone.caminati@bvtech.com>
2024-09-09 17:34:21 +0200

    fix: button style on firefox inside response page

--- src/routes/PaymentResponsePageV2.tsx
+++ src/routes/PaymentResponsePageV2.tsx
@@ -116,7 +116,7 @@ export default function PaymentResponsePageV2() {
               ? t(outcomeMessage.body, usefulPrintData)
               : ""}
           </Typography>
-          <Box px={8} sx={{ width: "100%", height: "100%" }}>
+          <Box display="block" px={8} sx={{ width: "100%", height: "100%" }}>
             <Button
               variant="outlined"
               onClick={() => {
@@ -124,7 +124,6 @@ export default function PaymentResponsePageV2() {
               }}
               sx={{
                 width: "100%",
-                height: "100%",
                 minHeight: 45,
                 my: 4,
               }}
