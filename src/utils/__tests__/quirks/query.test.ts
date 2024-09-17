1dc085a78bc6b3a249e3b5b8cfea56d93d57975f
Giovanni Berti <giovanni.berti@pagopa.it>
2024-09-17 15:49:07 +0200

    feat: add recovering of invalidly user-manipulated URLs

---
+++ src/utils/__tests__/quirks/query.test.ts
@@ -0,0 +1,24 @@
+import { sanitizeSearchParams } from "../../quirks/query";
+
+describe("Querystring quirks", () => {
+  it("should leave valid search params intact", () => {
+    const validSearchParams = new URLSearchParams({ a: "b", c: "d" });
+    expect(sanitizeSearchParams("?" + validSearchParams.toString())).toEqual(
+      validSearchParams
+    );
+  });
+
+  it("should recover search params with question mark instead of ampersand", () => {
+    const malformedSearchParams = "?foo=bar&quux=jk?aa=bb&cc=dd?ee=ff";
+    const recoveredSearchParams = new URLSearchParams({
+      foo: "bar",
+      quux: "jk",
+      aa: "bb",
+      cc: "dd",
+      ee: "ff",
+    });
+    expect(sanitizeSearchParams(malformedSearchParams)).toEqual(
+      recoveredSearchParams
+    );
+  });
+});
