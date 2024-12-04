19c3c5d412901cbea606dead935727ddd90fca86
EmanueleBVtech <emanuele.serrao@bvtech.com>
2024-12-04 15:16:24 +0100

    Update SkipToContent.tsx

--- src/components/commons/SkipToContent.tsx
+++ src/components/commons/SkipToContent.tsx
@@ -12,7 +12,7 @@ const SkipToContent = () => {
   const linkStyle: any = {
     transition: "opacity 0.2s ease-out",
     opacity: isFocused ? "1" : "0",
-    cursor: isFocused ? "" : "unset",
+    cursor: isFocused ? "" : "default",
   };
 
   return (
