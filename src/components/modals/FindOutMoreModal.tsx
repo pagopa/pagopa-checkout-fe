2f97937a06d7bc319474db25381120c4b448ecdf
e.serrao <emanuele.serrao@bvtech.com>
2024-12-16 16:35:57 +0100

    fixed styles on modal

--- src/components/modals/FindOutMoreModal.tsx
+++ src/components/modals/FindOutMoreModal.tsx
@@ -40,45 +40,59 @@ function FindOutMoreModal(props: IFindOutModeProps) {
           {t("findOutMoreModal.title")}
         </Typography>
       </DialogTitle>
-      <DialogContent sx={{ p: 4 }}>
-        <Typography variant="body2" component={"div"} sx={{ mb: 2 }}>
-          <Box display={"flex"} flexDirection={"column"} gap={"1em"}>
+      <DialogContent sx={{ p: 2 }}>
+        <Box display={"flex"} flexDirection={"column"} gap={"1em"}>
+          <div>
+            <Typography sx={{ fontWeight: "bold" }}>
+              {t("findOutMoreModal.section1Title")}
+            </Typography>
             <div>
-              <strong> {t("findOutMoreModal.section1Title")}</strong>
-              <div>
-                <div>{t("findOutMoreModal.section1ContentRow1")}</div>
-                <span> {t("findOutMoreModal.section1ContentRow2")}</span>
-                <Link
-                  target="_blank"
-                  href="https://www.pagopa.gov.it/it/cittadini/dove-pagare/"
-                >
-                  {t("findOutMoreModal.section1ContentRow2Link")}
-                </Link>
-              </div>
+              <Typography>
+                {t("findOutMoreModal.section1ContentRow1")}
+              </Typography>
+              <Typography sx={{ display: "inline" }}>
+                {t("findOutMoreModal.section1ContentRow2")}
+              </Typography>
+              <Link
+                target="_blank"
+                href="https://www.pagopa.gov.it/it/cittadini/dove-pagare/"
+              >
+                {t("findOutMoreModal.section1ContentRow2Link")}
+              </Link>
             </div>
+          </div>
+          <div>
+            <Typography sx={{ fontWeight: "bold" }}>
+              {t("findOutMoreModal.section2Title")}
+            </Typography>
             <div>
-              <strong> {t("findOutMoreModal.section2Title")}</strong>
-              <div>
-                <div>{t("findOutMoreModal.section2ContentRow1")}</div>
-                <div>{t("findOutMoreModal.section2ContentRow2")}</div>
-              </div>
+              <Typography>
+                {t("findOutMoreModal.section2ContentRow1")}
+              </Typography>
+              <Typography>
+                {t("findOutMoreModal.section2ContentRow2")}
+              </Typography>
             </div>
+          </div>
+          <div>
+            <Typography sx={{ fontWeight: "bold" }}>
+              {t("findOutMoreModal.section3Title")}
+            </Typography>
             <div>
-              <strong> {t("findOutMoreModal.section3Title")}</strong>
-              <div>
-                <span>{t("findOutMoreModal.section3ContentRow1")}</span>
-                <Link
-                  target={"_blank"}
-                  href={"https://www.pagopa.gov.it/it/assistenza/"}
-                >
-                  {t("findOutMoreModal.section3ContentRow2Link")}
-                </Link>
-              </div>
+              <Typography sx={{ display: "inline" }}>
+                {t("findOutMoreModal.section3ContentRow1")}
+              </Typography>
+              <Link
+                target={"_blank"}
+                href={"https://www.pagopa.gov.it/it/assistenza/"}
+              >
+                {t("findOutMoreModal.section3ContentRow2Link")}
+              </Link>
             </div>
-          </Box>
-        </Typography>
+          </div>
+        </Box>
       </DialogContent>
-      <DialogActions>
+      <DialogActions sx={{ p: 2 }}>
         <Button
           variant={"contained"}
           onClick={() => {
