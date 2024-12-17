60187399c6d9ae57e9b777d019e241aefd71076f
e.serrao <emanuele.serrao@bvtech.com>
2024-12-17 10:26:19 +0100

    evaluateOutcomeStatus added on evaluateClosePaymentResultError logic

--- src/utils/transactions/TransactionResultUtil.ts
+++ src/utils/transactions/TransactionResultUtil.ts
@@ -179,7 +179,10 @@ export const getViewOutcomeFromEcommerceResultCode: GetViewOutcomeFromEcommerceR
   // eslint-disable-next-line complexity
   (transactionStatus, nodeInfo, gatewayInfo): ViewOutcomeEnum => {
     if (nodeInfo?.closePaymentResultError) {
-      return evaluateClosePaymentResultError(nodeInfo?.closePaymentResultError);
+      return evaluateOutcomeStatus(
+        gatewayInfo,
+        evaluateClosePaymentResultError(nodeInfo?.closePaymentResultError)
+      );
     }
     switch (transactionStatus) {
       case TransactionStatusEnum.NOTIFIED_OK:
