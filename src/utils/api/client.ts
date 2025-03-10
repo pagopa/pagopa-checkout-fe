8e87a80999c871450d77870f8a408cf025682ed1
scaminati-bv <simone.caminati@bvtech.com>
2025-03-10 16:46:26 +0100

    fix: get user retry optimization

--- src/utils/api/client.ts
+++ src/utils/api/client.ts
@@ -82,7 +82,7 @@ export const apiCheckoutAuthServiceClientV1 = createAuthServiceClient({
 });
 
 /**
- * Api client for checkout auth service API V1 with retry with all error status, except 401 and 429
+ * Api client for checkout auth service API V1 with retry with 5xx error status
  */
 export const apiCheckoutAuthServiceClientGetUserV1 = createAuthServiceClient({
   baseUrl: conf.CHECKOUT_PAGOPA_APIM_HOST,
@@ -91,10 +91,6 @@ export const apiCheckoutAuthServiceClientGetUserV1 = createAuthServiceClient({
     fetch,
     conf.CHECKOUT_API_TIMEOUT as Millisecond,
     3,
-    (response: Response) =>
-      response.status < 200 &&
-      response.status >= 400 &&
-      response.status !== 401 &&
-      response.status !== 429
+    (response: Response) => response.status >= 500
   ),
 });
