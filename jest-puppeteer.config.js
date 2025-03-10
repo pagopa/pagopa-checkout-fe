e2adcd0a358a32343e488707071f452c39cbf5ee
Francesco Di Tonno <francesco.ditonno@bvtech.com>
2025-03-10 15:17:28 +0100

    fix: rmoved devtools in jest-puppeteer.config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,7 +3,6 @@ module.exports = {
         dumpio: true,
         headless: "new",
         slowMo: 50,
-        devtools: false,
         product: 'chrome',
         args: ["--no-sandbox"] // to fix
     },
