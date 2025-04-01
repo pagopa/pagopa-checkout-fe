ea65362d82f573c084da0722a283d583c960e9d4
Simone Infante <52280205+infantesimone@users.noreply.github.com>
2025-04-01 21:47:52 +0200

    fix: import related testing refactoring

--- src/utils/testing/testUtils.ts
+++ src/utils/testing/testUtils.ts
@@ -1,9 +1,9 @@
 import {
   PaymentCodeTypeEnum,
   PaymentInstrumentsType,
-} from "../features/payment/models/paymentModel";
-import { PaymentMethodStatusEnum } from "../../generated/definitions/payment-ecommerce/PaymentMethodStatus";
-import { PaymentMethodManagementTypeEnum } from "../../generated/definitions/payment-ecommerce/PaymentMethodManagementType";
+} from "../../features/payment/models/paymentModel";
+import { PaymentMethodStatusEnum } from "../../../generated/definitions/payment-ecommerce/PaymentMethodStatus";
+import { PaymentMethodManagementTypeEnum } from "../../../generated/definitions/payment-ecommerce/PaymentMethodManagementType";
 
 export function getPaymentMethodsMock(): Array<PaymentInstrumentsType> {
   return [
