c24a233450b230e90a44e6d611d707743c8e8e4d
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2025-04-01 15:50:30 +0200

    include jest libraries

--- jest.config.js
+++ jest.config.js
@@ -17,5 +17,23 @@ module.exports = {
   },
   moduleNameMapper: {
       "^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+      "src/**/*.ts",
+      "!<rootDir>/src/index.ts",
+      "!<rootDir>/src/instrumentation.ts",
+      "!<rootDir>/src/__tests__/**/*",
+      "!<rootDir>/src/__mocks__/**/*"
+  ],
+  coveragePathIgnorePatterns: ["index.ts"],
+  testResultsProcessor: "jest-sonar-reporter",
+  coveragePathIgnorePatterns: [
+      "node_modules",
+      "test-config",
+      ".module.ts",
+      "<rootDir>/src/generated/",
+      "<rootDir>/src/__mocks__/*.ts"
+  ],
+  coverageDirectory: "<rootDir>/coverage/"
 };
