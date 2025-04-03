fa1c4638876a3db55928b8083a4ab403d05abcf4
e.serrao <emanuele.serrao@bvtech.com>
2025-04-03 07:30:40 +0200

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { App } from "./App";
 import store from "./redux/store";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
