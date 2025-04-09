fef3855083ba02908606f08c30213ceda5c82a43
scaminati-bv <simone.caminati@bvtech.com>
2025-04-09 11:33:16 +0200

    feat: add helper unit tests

---
+++ src/utils/__tests__/api/helpers/checkoutAuthServiceHelper.test.tsx
@@ -0,0 +1,331 @@
+import ReCAPTCHA from "react-google-recaptcha";
+import { waitFor } from "@testing-library/react";
+import { mockApiConfig } from "../../../testUtils";
+import {
+  apiCheckoutAuthServiceClientAuthTokenV1,
+  apiCheckoutAuthServiceClientV1,
+  apiCheckoutAuthServiceWithRetryV1,
+} from "../../../api/client";
+import {
+  proceedToLogin,
+  authentication,
+  checkLogout,
+  logoutUser,
+  retrieveUserInfo,
+} from "../../../api/helper";
+import { ErrorsType } from "../../../../utils/errors/checkErrorsModel";
+import { callRecaptcha } from "../../../../utils/api/helpers/recaptchaHelper";
+import { getSessionItem } from "../../../../utils/storage/sessionStorage";
+
+jest.mock("../../../storage/sessionStorage", () => ({
+  getSessionItem: jest.fn(),
+  getRptIdsFromSession: jest.fn(),
+  SessionItems: {
+    authToken: "authToken",
+  },
+}));
+
+jest.mock("../../../config/config", () => ({
+  getConfigOrThrow: jest.fn(() => mockApiConfig),
+}));
+
+jest.mock("../../../api/client", () => ({
+  apiCheckoutAuthServiceClientV1: {
+    authLogin: jest.fn(),
+  },
+  apiCheckoutAuthServiceClientAuthTokenV1: {
+    authenticateWithAuthToken: jest.fn(),
+  },
+  apiCheckoutAuthServiceWithRetryV1: {
+    authUsers: jest.fn(),
+    authLogout: jest.fn(),
+  },
+}));
+
+jest.mock("../../../api/helpers/recaptchaHelper", () => ({
+  callRecaptcha: jest.fn(() => "recaptchaToken"),
+}));
+
+const mockOnResponse: jest.Mock = jest.fn();
+const mockOnError: jest.Mock = jest.fn();
+const onLogoutCallBack: jest.Mock = jest.fn();
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+describe("Checkout auth service helper - proceedToLogin tests", () => {
+  it("Should call onResponse when api return correct value", async () => {
+    const urlRedirect = "http://redirect-url";
+    (apiCheckoutAuthServiceClientV1.authLogin as jest.Mock).mockReturnValue(
+      Promise.resolve({
+        right: {
+          status: 200,
+          value: { urlRedirect },
+        },
+      })
+    );
+    await proceedToLogin({
+      recaptchaRef: {} as ReCAPTCHA,
+      onError: mockOnError,
+      onResponse: mockOnResponse,
+    });
+    expect(mockOnResponse).toHaveBeenCalledWith(urlRedirect);
+  });
+
+  it("Should call onError with ErrorsType.SERVER when api fail", async () => {
+    (apiCheckoutAuthServiceClientV1.authLogin as jest.Mock).mockReturnValue(
+      Promise.reject("Api error")
+    );
+    await proceedToLogin({
+      recaptchaRef: {} as ReCAPTCHA,
+      onError: mockOnError,
+      onResponse: mockOnResponse,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.SERVER);
+  });
+
+  it("Should call onError with ErrorsType.GENERIC_ERROR when api return 5xx", async () => {
+    (apiCheckoutAuthServiceClientV1.authLogin as jest.Mock).mockReturnValue(
+      Promise.resolve({
+        right: {
+          status: 500,
+        },
+      })
+    );
+    await proceedToLogin({
+      recaptchaRef: {} as ReCAPTCHA,
+      onError: mockOnError,
+      onResponse: mockOnResponse,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.GENERIC_ERROR);
+  });
+
+  it("Should call onError with ErrorsType.GENERIC_ERROR when recaptcha fail", async () => {
+    (callRecaptcha as jest.Mock).mockReturnValue(Promise.resolve(null));
+    await proceedToLogin({
+      recaptchaRef: {} as ReCAPTCHA,
+      onError: mockOnError,
+      onResponse: mockOnResponse,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.GENERIC_ERROR);
+  });
+});
+
+describe("Checkout auth service helper - authentication tests", () => {
+  it("Should call onResponse with the authToken value", async () => {
+    const authToken = "auth-token";
+    (
+      apiCheckoutAuthServiceClientAuthTokenV1.authenticateWithAuthToken as jest.Mock
+    ).mockReturnValue(
+      Promise.resolve({
+        right: {
+          status: 200,
+          value: { authToken },
+        },
+      })
+    );
+    await authentication({
+      authCode: "code",
+      state: "state",
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnResponse).toHaveBeenCalledWith(authToken);
+  });
+
+  it("Should call onError with ErrorsType.GENERIC_ERROR when api fail", async () => {
+    (
+      apiCheckoutAuthServiceClientAuthTokenV1.authenticateWithAuthToken as jest.Mock
+    ).mockReturnValue(Promise.reject("Api error"));
+    await authentication({
+      authCode: "code",
+      state: "state",
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.GENERIC_ERROR);
+  });
+
+  it("Should call onError with ErrorsType.CONNECTION when api return 5xx", async () => {
+    (
+      apiCheckoutAuthServiceClientAuthTokenV1.authenticateWithAuthToken as jest.Mock
+    ).mockReturnValue(
+      Promise.resolve({
+        right: {
+          status: 500,
+        },
+      })
+    );
+    await authentication({
+      authCode: "code",
+      state: "state",
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.CONNECTION);
+  });
+
+  it("Should call onError with ErrorsType.GENERIC_ERROR when authCode missing", async () => {
+    await authentication({
+      authCode: null,
+      state: "state",
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.GENERIC_ERROR);
+  });
+
+  it("Should call onError with ErrorsType.GENERIC_ERROR when state missing", async () => {
+    await authentication({
+      authCode: "authCode",
+      state: null,
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.GENERIC_ERROR);
+  });
+});
+
+describe("Checkout auth service helper - checkLogout tests", () => {
+  it("Should call onLogoutCallBack on logoutUser success", async () => {
+    (getSessionItem as jest.Mock).mockReturnValue("authToken");
+    (apiCheckoutAuthServiceWithRetryV1.authLogout as jest.Mock).mockReturnValue(
+      Promise.resolve({
+        right: {
+          status: 204,
+          value: {},
+        },
+      })
+    );
+    checkLogout(onLogoutCallBack);
+    await waitFor(() => {
+      expect(onLogoutCallBack).toHaveBeenCalled();
+    });
+  });
+
+  it("Should call onLogoutCallBack on logoutUser error", async () => {
+    (getSessionItem as jest.Mock).mockReturnValue("authToken");
+    (apiCheckoutAuthServiceWithRetryV1.authLogout as jest.Mock).mockReturnValue(
+      Promise.reject("Api error")
+    );
+    checkLogout(onLogoutCallBack);
+    expect(onLogoutCallBack).toHaveBeenCalled();
+  });
+});
+
+describe("Checkout auth service helper - logoutUser tests", () => {
+  it("Should call onResponse when logout is completed", async () => {
+    (getSessionItem as jest.Mock).mockReturnValue("authToken");
+    (apiCheckoutAuthServiceWithRetryV1.authLogout as jest.Mock).mockReturnValue(
+      Promise.resolve({
+        right: {
+          status: 204,
+          value: {},
+        },
+      })
+    );
+    await logoutUser({
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnResponse).toHaveBeenCalled();
+  });
+
+  it("Should call onError with ErrorsType.GENERIC_ERROR when api fail", async () => {
+    (getSessionItem as jest.Mock).mockReturnValue("authToken");
+    (apiCheckoutAuthServiceWithRetryV1.authLogout as jest.Mock).mockReturnValue(
+      Promise.reject("Api error")
+    );
+    await logoutUser({
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.GENERIC_ERROR);
+  });
+
+  it("Should call onError with ErrorsType.CONNECTION when api return 5xx", async () => {
+    (getSessionItem as jest.Mock).mockReturnValue("authToken");
+    (apiCheckoutAuthServiceWithRetryV1.authLogout as jest.Mock).mockReturnValue(
+      Promise.resolve({
+        right: {
+          status: 500,
+        },
+      })
+    );
+    await logoutUser({
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.CONNECTION);
+  });
+
+  it("Should call onError with ErrorsType.GENERIC_ERROR when authToken on session is missing", async () => {
+    (getSessionItem as jest.Mock).mockReturnValue(null);
+    await logoutUser({
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.GENERIC_ERROR);
+  });
+});
+
+describe("Checkout auth service helper - retrieveUserInfo tests", () => {
+  it("Should call onResponse when authUsers is completed", async () => {
+    const userInfo = {
+      name: "NomeTest",
+      familyName: "CognomeTest",
+    };
+    (getSessionItem as jest.Mock).mockReturnValue("authToken");
+    (apiCheckoutAuthServiceWithRetryV1.authUsers as jest.Mock).mockReturnValue(
+      Promise.resolve({
+        right: {
+          status: 200,
+          value: userInfo,
+        },
+      })
+    );
+    await retrieveUserInfo({
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnResponse).toHaveBeenCalledWith(userInfo);
+  });
+
+  it("Should call onError with ErrorsType.GENERIC_ERROR when api fail", async () => {
+    (getSessionItem as jest.Mock).mockReturnValue("authToken");
+    (apiCheckoutAuthServiceWithRetryV1.authUsers as jest.Mock).mockReturnValue(
+      Promise.reject("Api error")
+    );
+    await retrieveUserInfo({
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.GENERIC_ERROR);
+  });
+
+  it("Should call onError with ErrorsType.CONNECTION when api return 5xx", async () => {
+    (getSessionItem as jest.Mock).mockReturnValue("authToken");
+    (apiCheckoutAuthServiceWithRetryV1.authUsers as jest.Mock).mockReturnValue(
+      Promise.resolve({
+        right: {
+          status: 500,
+        },
+      })
+    );
+    await retrieveUserInfo({
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.CONNECTION);
+  });
+
+  it("Should call onError with ErrorsType.GENERIC_ERROR when authToken on session is missing", async () => {
+    (getSessionItem as jest.Mock).mockReturnValue(null);
+    await retrieveUserInfo({
+      onResponse: mockOnResponse,
+      onError: mockOnError,
+    });
+    expect(mockOnError).toHaveBeenCalledWith(ErrorsType.GENERIC_ERROR);
+  });
+});
---
+++ src/utils/__tests__/api/helpers/checkoutFeatureFlagsHelper.test.ts
@@ -0,0 +1,52 @@
+import { mockApiConfig } from "../../../../utils/testUtils";
+import { apiCheckoutFeatureFlags } from "../../../api/client";
+import { evaluateFeatureFlag } from "../../../api/helper";
+
+jest.mock("../../../config/config", () => ({
+  getConfigOrThrow: jest.fn(() => mockApiConfig),
+}));
+
+jest.mock("../../../api/client", () => ({
+  apiCheckoutFeatureFlags: {
+    evaluateFeatureFlags: jest.fn(),
+  },
+}));
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+describe("Checkout feature flags helper test", () => {
+  const mockOnResponse: jest.Mock = jest.fn();
+  const mockOnError: jest.Mock = jest.fn();
+
+  it("Should call onResponse with the correct feature flag value", async () => {
+    (apiCheckoutFeatureFlags.evaluateFeatureFlags as jest.Mock).mockReturnValue(
+      Promise.resolve({
+        right: {
+          value: {
+            isFeatureFlagEnabled: true,
+          },
+        },
+      })
+    );
+    await evaluateFeatureFlag(
+      "isFeatureFlagEnabled",
+      mockOnError,
+      mockOnResponse
+    );
+    expect(mockOnResponse).toHaveBeenCalledWith({ enabled: true });
+  });
+
+  it("Should call onError when api fail", async () => {
+    (apiCheckoutFeatureFlags.evaluateFeatureFlags as jest.Mock).mockReturnValue(
+      Promise.reject("Api error")
+    );
+    await evaluateFeatureFlag(
+      "isFeatureFlagEnabled",
+      mockOnError,
+      mockOnResponse
+    );
+    expect(mockOnError).toHaveBeenCalledWith("Network error");
+  });
+});
