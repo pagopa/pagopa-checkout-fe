7869c5babef2e927e748339fceb9955d69bcff3c
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2025-05-08 14:21:52 +0200

    feat(polling): [CHK-4110] remove unused code

--- src/utils/transactions/transactionHelper.ts
+++ src/utils/transactions/transactionHelper.ts
@@ -2,7 +2,6 @@
 import * as E from "fp-ts/Either";
 import * as TE from "fp-ts/TaskEither";
 import { pipe } from "fp-ts/function";
-import { Client as EcommerceClient } from "../../../generated/definitions/payment-ecommerce-v2/client";
 import { Client as EcommerceClientV1 } from "../../../generated/definitions/payment-ecommerce/client";
 import {
   TRANSACTION_POLLING_CHECK_INIT,
@@ -12,67 +11,8 @@ import {
   TRANSACTION_POLLING_CHECK_RESP_ERR,
 } from "../config/mixpanelDefs";
 import { mixpanel } from "../config/mixpanelHelperInit";
-import { TransactionInfo } from "../../../generated/definitions/payment-ecommerce-v2/TransactionInfo";
 import { TransactionOutcomeInfo } from "../../../generated/definitions/payment-ecommerce/TransactionOutcomeInfo";
 import { UNKNOWN } from "./TransactionStatesTypes";
-
-export const ecommerceTransaction = (
-  transactionId: string,
-  bearerAuth: string,
-  ecommerceClient: EcommerceClient
-): TE.TaskEither<UNKNOWN, TransactionInfo> => {
-  mixpanel.track(TRANSACTION_POLLING_CHECK_INIT.value, {
-    EVENT_ID: TRANSACTION_POLLING_CHECK_INIT.value,
-  });
-  return pipe(
-    TE.tryCatch(
-      () =>
-        ecommerceClient.getTransactionInfo({
-          bearerAuth,
-          transactionId,
-        }),
-      () => {
-        mixpanel.track(TRANSACTION_POLLING_CHECK_NET_ERR.value, {
-          EVENT_ID: TRANSACTION_POLLING_CHECK_NET_ERR.value,
-        });
-        return E.toError;
-      }
-    ),
-    TE.fold(
-      () => {
-        mixpanel.track(TRANSACTION_POLLING_CHECK_SVR_ERR.value, {
-          EVENT_ID: TRANSACTION_POLLING_CHECK_SVR_ERR.value,
-        });
-        return TE.left(UNKNOWN.value);
-      },
-      (errorOrResponse) =>
-        pipe(
-          errorOrResponse,
-          E.fold(
-            () => {
-              mixpanel.track(TRANSACTION_POLLING_CHECK_RESP_ERR.value, {
-                EVENT_ID: TRANSACTION_POLLING_CHECK_RESP_ERR.value,
-              });
-              return TE.left(UNKNOWN.value);
-            },
-            (responseType) => {
-              if (responseType.status === 200) {
-                mixpanel.track(TRANSACTION_POLLING_CHECK_SUCCESS.value, {
-                  EVENT_ID: TRANSACTION_POLLING_CHECK_SUCCESS.value,
-                });
-                return TE.of(responseType.value);
-              } else {
-                mixpanel.track(TRANSACTION_POLLING_CHECK_RESP_ERR.value, {
-                  EVENT_ID: TRANSACTION_POLLING_CHECK_RESP_ERR.value,
-                });
-                return TE.left(UNKNOWN.value);
-              }
-            }
-          )
-        )
-    )
-  );
-};
 
 export const ecommerceTransactionOutcome = (
   transactionId: string,
