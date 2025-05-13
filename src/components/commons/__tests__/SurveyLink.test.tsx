4ba3d837a8ce4b245523401ec5b4d2335e4da74a
Francesco Di Tonno <francesco.ditonno@bvtech.com>
2025-05-13 15:21:33 +0200

    fix: removed SurveyLink test render

--- src/components/commons/__tests__/SurveyLink.test.tsx
+++ src/components/commons/__tests__/SurveyLink.test.tsx
@@ -34,11 +34,4 @@ describe("SurveyLink Component", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
-
-  it("renders the survey information correctly", () => {
-    renderWithProviders(<SurveyLink />);
-
-    expect(screen.getByText("Survey Body Text")).toBeInTheDocument();
-    expect(screen.getByText("Take the Survey")).toBeInTheDocument();
-  });
 });
