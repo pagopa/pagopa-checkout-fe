9d9eda9a75a046ce76c46806e92517fac76fe679
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2025-06-26 16:50:15 +0200

    update library

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { App } from "./App";
 import store from "./redux/store";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
