f7f07d1576907a3bdde5bca5a53424989e3dc165
Forciniti Luigi <luigi.forciniti.ext@jakala.com>
2025-07-04 15:53:30 +0200

    Update jest-puppeteer.config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     launch: {
         dumpio: true,
-        headless:false,
+        headless:'new',
         product: 'chrome',
         args: ["--no-sandbox"] // to fix
     },
