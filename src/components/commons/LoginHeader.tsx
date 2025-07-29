c7dc59553ab8db38b74b62267daa477ba3e57d1e
matteo-samoila-jakala <matteo.samoila@jakala.com>
2025-07-29 16:03:21 +0200

    fix: [PIDM-608] Login Button Translation

--- src/components/commons/LoginHeader.tsx
+++ src/components/commons/LoginHeader.tsx
@@ -243,6 +243,7 @@ export default function LoginHeader() {
         enableAssistanceButton={false}
         onAssistanceClick={() => {}} // eslint-disable-line @typescript-eslint/no-empty-function
         onLogin={handleClickOnLogin}
+        translationsMap={{logIn: t("authExpiredPage.buttons.login")}}
       />
       <Box display="none">
         <ReCAPTCHA
