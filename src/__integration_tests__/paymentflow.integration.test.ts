70016b78bab7216bdc321fab395f335977cae460
Pietro Tota <pietro.tota@pagopa.it>
2025-09-23 13:08:12 +0200

    chore: enable all tests

--- src/__integration_tests__/paymentflow.integration.test.ts
+++ src/__integration_tests__/paymentflow.integration.test.ts
@@ -590,7 +590,7 @@ describe("Checkout Payment - PSP Selection Flow", () => {
 });
 
 
-describe.only("Payment Methods list tests - Fee rendering", () => {
+describe("Payment Methods list tests - Fee rendering", () => {
   // mock PaymentMethodsResponse v2
   const mockPaymentMethodsV2 = {
     paymentMethods: [
