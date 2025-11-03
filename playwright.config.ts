6acc2d97e1de0ebb40c12d90beb4fad8fbc369a3
Forciniti Luigi <luigi.forciniti.ext@jakala.com>
2025-11-03 16:41:19 +0100

    chore: configure JUnit reporter for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   testMatch: ["test/spec/**/*.spec.ts"],
   retries: 1,
   workers: process.env.CI ? 4 : undefined,
-  reporter: [["dot"]],
+  reporter: [
+    ["dot"],
+    ["junit", { outputFile: "test-results/results.xml" }], // reporter globale
+  ],
   use: {
     headless: true, // Run tests in headless mode
     viewport: { width: 1280, height: 720 }, // Default viewport size
@@ -17,10 +20,14 @@ export default defineConfig({
   },
   projects: [
     {
-      name: "chromium",
-      use: {
-        browserName: "chromium",
-      },
+      name: "payment-flow",
+      testMatch: ["test/spec/paymentflow.integration.spec.ts"],
+      use: { browserName: "chromium" },
+    },
+    {
+      name: "outcomes",
+      testMatch: ["test/spec/outcomes.integration.spec.ts"],
+      use: { browserName: "chromium" },
     },
   ],
 });
