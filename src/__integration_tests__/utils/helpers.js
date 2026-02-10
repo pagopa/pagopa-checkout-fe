c4feb6f1aa4e9ff734650c1619ae78dde35fa087
Francesco Di Tonno <francesco.ditonno.ext@jakala.com>
2026-02-10 12:15:38 +0100

    test: removed old impl

--- src/__integration_tests__/utils/helpers.js
+++ src/__integration_tests__/utils/helpers.js
@@ -294,25 +294,6 @@ export const tryHandlePspPickerPage = async ()=>{
   }
 }
 
-/*
-export const selectPspOnPspPickerPage = async () => {
-  try{
-    const pspPickerRadio = await page.waitForSelector("#psp-radio-button-unchecked", {
-      visible: true, timeout: 500
-    });
-    await pspPickerRadio.click();
-  
-    const continueButton = await page.waitForSelector("#paymentPspListPageButtonContinue", {
-      visible: true, timeout: 500
-    });
-    
-    await continueButton.click();
-  }catch(e){
-    console.log("Buttons not found: this is caused by PSP page immediately navigate to the summary page (if 1 psp available)");
-  }
-}
-*/
-
 export const selectPspOnPspPickerPage = async () => {
   try {
     const firstPspCard = await page.waitForSelector("label.MuiFormControlLabel-root", {
