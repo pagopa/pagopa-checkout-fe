77173f5e08e20e832f0e3af7fd15fee1c075d636
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2026-02-17 11:40:04 +0100

    PIDM-1010 fix reader for select

--- src/components/LanguageMenu/LanguageNativeSelect.tsx
+++ src/components/LanguageMenu/LanguageNativeSelect.tsx
@@ -1,5 +1,4 @@
-import { Box, InputBase, NativeSelect, styled, useTheme } from "@mui/material";
-import { visuallyHidden } from "@mui/utils";
+import { InputBase, NativeSelect, styled, useTheme } from "@mui/material";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import supportedLang, { getSortedLang } from "../../translations/lang";
@@ -9,13 +8,6 @@ export default function LanguageNativeSelect() {
   const { i18n, t } = useTranslation();
   const theme = useTheme();
   const [lang, setLang] = React.useState<string>(i18n.language.split("-")[0]);
-
-  const currentLanguageLabel = (lang: string) => {
-    const languages = getSortedLang();
-    const currLang = languages.find((elem) => elem.lang.split("-")[0] === lang);
-    // currLang guaranteed to exist
-    return currLang ? currLang.label : "";
-  };
 
   const languages = getSortedLang().map((elem, index) => (
     <option key={index} value={elem.lang.split("-")[0]}>
@@ -45,7 +37,6 @@ export default function LanguageNativeSelect() {
 
   return (
     <>
-      <Box sx={{ ...visuallyHidden }}>{t("ariaLabels.languageMenu")}</Box>
       <NativeSelect
         id="languageMenu"
         defaultValue={lang in supportedLang ? lang : fallbackLang}
@@ -56,9 +47,9 @@ export default function LanguageNativeSelect() {
             color: theme.palette.text.primary,
           },
         }}
-        aria-label={currentLanguageLabel(
-          lang in supportedLang ? lang : fallbackLang
-        )}
+        inputProps={{
+          "aria-label": t("ariaLabels.languageMenu"),
+        }}
       >
         {languages}
       </NativeSelect>
