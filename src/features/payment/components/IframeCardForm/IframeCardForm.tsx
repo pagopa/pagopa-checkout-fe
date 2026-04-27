214f18cd2e74045501d8dda9661f657878780bcf
Francesco Di Tonno <francesco.ditonno.ext@jakala.com>
2026-04-27 15:50:13 +0200

    fix: validation on submit

--- src/features/payment/components/IframeCardForm/IframeCardForm.tsx
+++ src/features/payment/components/IframeCardForm/IframeCardForm.tsx
@@ -292,33 +292,53 @@ export default function IframeCardForm(props: Props) {
   }, [form?.orderId]);
 
   const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const nextFormStatus = Object.values(IdFields).reduce<FormStatus>(
+      (acc, fieldId) => ({
+        ...acc,
+        [fieldId]:
+          formStatus[fieldId].isValid === true
+            ? formStatus[fieldId]
+            : {
+                isValid: false,
+                errorCode: "REQUIRED",
+                errorMessage: t("inputCardPage.formFields.required"),
+              },
+      }),
+      {} as FormStatus
+    );
+
+    setFormStatus(nextFormStatus);
+
+    if (!formIsValid(nextFormStatus)) {
+      focusOnError(nextFormStatus);
+      return;
+    }
+
     try {
-      e.preventDefault();
-      if (formIsValid(formStatus)) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        buildInstance.confirmData(() => setLoading(true));
-      } else {
-        focusOnError();
-      }
-    } catch (e) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      buildInstance.confirmData(() => setLoading(true));
+    } catch {
       onError(ErrorsType.GENERIC_ERROR);
     }
   };
 
-  const focusOnError = () => {
-    for (const fieldId of Object.keys(IdFields)) {
-      const fieldStatus = formStatus[fieldId as FieldId];
+  const focusOnError = (status: FormStatus = formStatus) => {
+    for (const fieldId of Object.values(IdFields)) {
+      const fieldStatus = status[fieldId];
+
       if (fieldStatus?.isValid === false) {
         const iframe = document.getElementById(`frame_${fieldId}`);
-        if (iframe && iframe instanceof HTMLIFrameElement) {
+
+        if (iframe instanceof HTMLIFrameElement) {
           iframe.focus();
           break;
         }
       }
     }
   };
-
   const { t } = useTranslation();
 
   return (
